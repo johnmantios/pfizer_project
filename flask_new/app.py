1848d8ca5a6bf33696b750e83d81741050805ad4
karolosk <karolos.koutsoulelos@gmail.com>
2020-09-26 17:49:12 +0300

    Added init db call in flask start/removed pycached files from git

--- flask_new/app.py
+++ flask_new/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-from controllers import (ping_controller,etl_controller,stats_cotroller,patient_controller)
+from controllers import (
+    ping_controller,
+    etl_controller,
+    stats_cotroller,
+    patient_controller,
+)
+from repo import db_utils
 
 app = Flask(__name__)
 CORS(app)
@@ -15,5 +21,9 @@ app.register_blueprint(ping_controller.api)
 app.register_blueprint(etl_controller.api)
 app.register_blueprint(stats_cotroller.api)
 app.register_blueprint(patient_controller.api)
+
+
 if __name__ == "__main__":
+
+    db_utils.init_db()
     app.run(debug=True)
